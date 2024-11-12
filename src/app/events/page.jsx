c00e94f76e094f4c70fb626b8db797ad199af873
gilsonsilven <gilsonsilven@gmail.com>
2024-11-12 18:31:40 -0300

    defining project pages

---
+++ src/app/events/page.jsx
@@ -0,0 +1,8 @@
+
+
+export default function Events() {
+
+    return (
+        <h1>eventos</h1>
+    )
+}
---
+++ src/app/homepage/page.jsx
@@ -0,0 +1,8 @@
+
+
+export default function Homepage() {
+
+    return (
+        <h1>página inicial depois de logar</h1>
+    )
+}
